b5d81b008f54482d92d43d54e0ae794ea76e3c62
lynw <carolyn.wang94@gmail.com>
2022-04-23 10:10:59 -0400

    first commit

---
+++ client.js
@@ -0,0 +1,29 @@
+const net = require("net");
+const {IP, PORT} = require("./variables");
+
+const connect = function () {
+
+  const conn = net.createConnection({
+    host: IP,
+    port: PORT 
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("connect", () => {  
+    console.log("Successfully connected to game server!");
+    conn.write("Name: CW_");
+  }); 
+
+  conn.on('data', (data) => {
+    console.log(data);
+  });
+
+
+  return conn;  
+};
+
+connect();
+
+module.exports = connect;
---
+++ input.js
@@ -0,0 +1,34 @@
+const {MOVE_UP_KEY, MOVE_DOWN_KEY, MOVE_LEFT_KEY, MOVE_RIGHT_KEY, CTRL_C} = require("./variables");
+
+
+let connection;
+
+
+const handleUserInput = function(key) {
+  if (key === CTRL_C) {
+    process.exit();
+  } else if (key === MOVE_UP_KEY) {
+    connection.write("Move: up");
+  } else if (key === MOVE_DOWN_KEY) {
+    connection.write("Move: down");
+  } else if (key === MOVE_LEFT_KEY) {
+    connection.write("Move: left");
+  } else if (key === MOVE_RIGHT_KEY) {
+    connection.write("Move: right");
+  } 
+};
+
+
+const setupInput = function (conn) {
+  connection = conn;
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+
+
+module.exports = setupInput;
