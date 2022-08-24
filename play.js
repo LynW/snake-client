b04d46252fa7b586310e3741adb4f30410c899ea
lynw <c54wang@gmail.com>
2022-08-24 20:59:28 +0000

    add host and port

--- play.js
+++ play.js
@@ -3,8 +3,8 @@ const net = require("net");
 // establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
-    host: // IP address here,
-    port: // PORT number here,
+    host: "10.0.2.15",// IP address here,
+    port: "50541",// PORT number here,
   });
 
   // interpret incoming data as text
