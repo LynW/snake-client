406f96a036173741dd1d7594ff27983e64918961
lynw <c54wang@gmail.com>
2022-08-25 17:04:57 +0000

    add constants

---
+++ constants.js
@@ -0,0 +1,14 @@
+const IP = "10.0.2.15";
+const PORT = "50541";
+const CONNECTION_MSG = "Connected!";
+const CLOSE_MSG = "Closing connection!";
+const NAME = "CW";
+
+
+module.exports = {
+  IP,
+  PORT,
+  CONNECTION_MSG,
+  NAME,
+  CLOSE_MSG
+};
